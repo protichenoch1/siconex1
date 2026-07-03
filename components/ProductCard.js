7c3ec2db69872ca624f0dab973c8d31065057456
protichenoch1 <protichenoch1@gmail.com>
2026-07-03 09:10:12 +0300

    Add image prop to ProductCard component

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,8 +1,12 @@
-export default function ProductCard({ name, price }) {
+export default function ProductCard({ name, price, image }) {
   return (
     <div className="card">
+      <img src={image} alt={name} />
+
       <h3>{name}</h3>
-      <p>{price}</p>
+
+      <p className="price">{price}</p>
+
       <button>Add to Cart</button>
     </div>
   );
