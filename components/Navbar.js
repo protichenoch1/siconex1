cef5c607271c0bccf83d4709c47b56441f3dff6b
protichenoch1 <protichenoch1@gmail.com>
2026-07-03 18:17:15 +0300

    Replace Link components with anchor tags for icons

--- components/Navbar.js
+++ components/Navbar.js
@@ -12,9 +12,25 @@ export default function Navbar() {
         </div>
 
         <div className="nav-right">
-          <Link href="/account" className="icon-box">👤</Link>
-          <Link href="/cart" className="icon-box">🛒</Link>
-        </div>
+
+  {/* ACCOUNT ICON */}
+  <a href="/account" className="icon">
+    <svg width="22" height="22" viewBox="0 0 24 24" fill="none">
+      <circle cx="12" cy="8" r="4" stroke="#0a8f3c" strokeWidth="2"/>
+      <path d="M4 20c0-4 4-6 8-6s8 2 8 6" stroke="#0a8f3c" strokeWidth="2"/>
+    </svg>
+  </a>
+
+  {/* CART ICON */}
+  <a href="/cart" className="icon">
+    <svg width="22" height="22" viewBox="0 0 24 24" fill="none">
+      <path d="M6 6h15l-2 9H8L6 6z" stroke="#0a8f3c" strokeWidth="2"/>
+      <circle cx="9" cy="20" r="1.5" fill="#0a8f3c"/>
+      <circle cx="18" cy="20" r="1.5" fill="#0a8f3c"/>
+    </svg>
+  </a>
+
+</div>
       </div>
 
       {/* SEARCH */}
