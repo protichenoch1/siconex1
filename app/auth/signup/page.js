72e74881267d0ceae7b6c37e664431f0aa68d1ae
protichenoch1 <protichenoch1@gmail.com>
2026-07-06 21:39:15 +0300

    Implement signup page with email and password

---
+++ app/auth/signup/page.js
@@ -0,0 +1,51 @@
+"use client";
+
+import { useState } from "react";
+import { supabase } from "@/lib/supabase";
+import { useRouter } from "next/navigation";
+
+export default function Signup() {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignup = async () => {
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+
+    if (error) {
+      alert(error.message);
+      return;
+    }
+
+    // auto login
+    await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    router.push("/");
+  };
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Create Account</h2>
+
+      <input
+        type="email"
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      /><br /><br />
+
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      /><br /><br />
+
+      <button onClick={handleSignup}>Sign Up</button>
+    </div>
+  );
+}
