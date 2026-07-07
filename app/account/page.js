13d2f0bca1ecc05a0dbf191a7ec895f1594ede62
protichenoch1 <protichenoch1@gmail.com>
2026-07-07 09:14:26 +0300

    Fix import path for ProtectedRoute component

--- app/account/page.js
+++ app/account/page.js
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import ProtectedRoute from "../../components/ProtectedRoute";
+import ProtectedRoute from "../components/ProtectedRoute";
 
 function AccountContent() {
   const [user, setUser] = useState(null);
