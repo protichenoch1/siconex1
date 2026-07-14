671da6e3ff3d9df6319efdf44e523d19d3d2afaf
protichenoch1 <protichenoch1@gmail.com>
2026-07-14 18:37:20 +0300

    Fix import path for products data

--- app/category/[id]/page.js
+++ app/category/[id]/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useParams, useRouter } from "next/navigation";
-import { products } from "../../data/products";
+import { products } from "../../../data/products";
 
 export default function CategoryPage() {
   const { id } = useParams(); // e.g. "phones"
